ba9b9fc49260570fe2d43a00ee3571f498d4c7a0
Pat_Away <dmtpatricia@gmail.com>
2025-11-08 15:09:39 +0100

    iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,25 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
+console.log(names.length);
 // Dichiara la funzione qui.
+/**
+ * Funzione che crea un array con elementi la prima letteera di ogni elemento dell'array
+ * @param {array} namesArray array contenente nomi
+ * @returns {arry} Array di elementi con un solo carattere
+ */
+function firstLetterArray(namesArray) {
+    let result = [];
 
+    for (let i=0; i<namesArray.length ; i++){
+        result[i] = namesArray[i].charAt(0);
+        console.log(result);
+    }
+    return result;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const risultato = firstLetterArray(names);
+console.log(risultato);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
