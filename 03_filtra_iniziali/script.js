f4d155a8977aa91c9267cc7d8ca66ff8630c29d3
Pat_Away <dmtpatricia@gmail.com>
2025-11-08 16:25:39 +0100

    filtraIniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,28 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+/**
+ * Funzione che restituisce un'array contenente le stringhe che hanno la lettera scelta come iniziale
+ * @param {Array} stringArray Array contenente stringhe
+ * @param {string} letter lettera scelta
+ * @returns {Array} restituisce l'array che rispetta i criteri iniziali
+ */
 
+function sameInitialArray(stringArray, letter) {
+    const result = [];
+    //stringArray.toLowerCase();
+    let j = 0;
+    for (let i = 0; i < stringArray.length; i++) {
+        if (letter.toUpperCase() === stringArray[i].toUpperCase().charAt(0)) { 
+            result[j] = stringArray[i];
+            j++;
+        }        
+    }
+    return result;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const letter = "A"
+console.log(sameInitialArray(names, letter))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
