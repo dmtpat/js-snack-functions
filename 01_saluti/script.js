ee002ec344b181563e0cfc9345dc78053f223c25
Pat_Away <dmtpatricia@gmail.com>
2025-11-08 11:50:06 +0100

    saluti

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,12 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function ciaoNome(nome) {
+    return (alert(`Ciao ${nome}`));
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+ciaoNome(userName);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
