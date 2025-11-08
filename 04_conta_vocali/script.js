275136f275897919fa23d1f7fa3ba453110ceab0
Pat_Away <dmtpatricia@gmail.com>
2025-11-08 22:19:47 +0100

    contaVocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,27 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+/**
+ * 
+ * @param {string} word parola da controllare
+ * @returns {number}
+ */
+
+function vovelCounter(word) {
+    //word.toUpperCase();
+    let c = 0;
+    for (let i = 0; i < word.length; i++) {
+        let currentChar = word.charAt(i).toUpperCase();
+        if(currentChar === "A" ||currentChar === "E" ||currentChar === "I" ||currentChar === "O" ||currentChar === "U") {
+            c++;
+        }  
+    }
+    return c;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(vovelCounter(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
